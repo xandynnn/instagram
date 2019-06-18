5a77d4491466ad004489b1c2f7c4dfa72e56f0bb
amattos <amattos@gm5.com.br>
2019-06-18 19:57:02 -0300

    updatge

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,42 +1,44 @@
-const Post = require('../models/Post');
 const Sharp = require('sharp');
 const path = require('path');
 const fs = require('fs');
+const Post = require('../models/Post');
 
 module.exports = {
-    async index( req, res){
-        const posts = await Post.find().sort('-createdAt');
-        return res.json(posts);
-    },
-    async store(req, res){
-        const { author, place, description, hashtags } = req.body;
-        const { filename: image } = req.file;
+  async index(req, res) {
+    const posts = await Post.find().sort('-createdAt');
+    return res.json(posts);
+  },
+  async store(req, res) {
+    const {
+ author, place, description, hashtags 
+} = req.body;
+    const { filename: image } = req.file;
 
-        const [name] = image.split('.');
-        const fileName = `${name}.jpg`
+    const [name] = image.split('.');
+    const fileName = `${name}.jpg`;
 
-        // Redimensionamento da imagem
-        await Sharp(req.file.path)
-            .resize(500)
-            .jpeg({quality: 70})
-            .toFile(
-                path.resolve(req.file.destination, 'resized', fileName )
-            )
-        
-        // Deleta a imagem não redimensionada
-        //fs.unlinkSync(req.file.path);
+    // Redimensionamento da imagem
+    await Sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(
+        path.resolve(req.file.destination, 'resized', fileName),
+      );
 
-        const post = await Post.create({
-            author,
-            place,
-            description,
-            hashtags,
-            image: fileName,
-        })
+    // Deleta a imagem não redimensionada
+    // fs.unlinkSync(req.file.path);
 
-        // socketio emite para todos os usuários com a nova informação em tempo real
-        req.io.emit('post', post);
+    const post = await Post.create({
+      author,
+      place,
+      description,
+      hashtags,
+      image: fileName,
+    });
 
-        return res.json(post);
-    }
-}
+    // socketio emite para todos os usuários com a nova informação em tempo real
+    req.io.emit('post', post);
+
+    return res.json(post);
+  },
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,10 +6,7 @@ const LikeController = require('./controllers/LikeController');
 
 const routes = new express.Router();
 const upload = multer(uploadConfig);
-
-routes.get('/posts', PostController.index );
-routes.post('/posts', upload.single('image'), PostController.store );
-
-routes.post('/posts/:id/like', LikeController.store );
-
+routes.get('/posts', PostController.index);
+routes.post('/posts', upload.single('image'), PostController.store);
+routes.post('/posts/:id/like', LikeController.store);
 module.exports = routes;
